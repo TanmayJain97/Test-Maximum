ebac1ea2c8a8f36df76dd38018cfae15c980de1f
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-06 21:29:47 +0530

    Add: Float_maximum method added

--- main/java/com/test_maximum/TestMaximumMain.java
+++ main/java/com/test_maximum/TestMaximumMain.java
@@ -9,10 +9,14 @@ public class TestMaximumMain {
 	public int maxNumber(Stream<Integer> stream) {
 		return stream.max(Integer::compareTo).get();
 	}
+	
+	public Float maxFloat(Stream<Float> stream) {
+		return stream.max(Float::compareTo).get();
+	}
 
 	public static void main( String[] args ) {
 		
-		TestMaximumMain buildObj = new TestMaximumMain();
+	TestMaximumMain buildObj = new TestMaximumMain();
     	Scanner sc= new Scanner(System.in);
     	System.out.println("Input 3 integers: ");
     	System.out.print("Int 1: ");
@@ -23,8 +27,11 @@ public class TestMaximumMain {
     	int num3 = sc.nextInt();
     	
     	Integer num_arr[] = new Integer[]{num1,num2,num3};
-    	Stream<Integer> stream=Arrays.stream(num_arr);
-    	System.out.println("Maximum integer = "+buildObj.maxNumber(stream));
+    	Float fl_arr[] = new Float[] {(float) num1,(float) num2,(float) num3};
+    	Stream<Integer> num_stream=Arrays.stream(num_arr);
+    	Stream<Float> fl_stream=Arrays.stream(fl_arr);
+    	System.out.println("Maximum integer = "+buildObj.maxNumber(num_stream));
+    	System.out.println("Maximum float = "+buildObj.maxFloat(fl_stream));
     	sc.close();
     }
 }
